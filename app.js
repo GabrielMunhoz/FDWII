018b8f603f3395803b9b378b08f3d414a4144436
gabrielmunhoz <gabriell_munhoz@icloud.com>
2022-08-18 21:55:20 -0300

    Adicionado acoes as rotas

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.json());
+
 const port = 3000
 
+let dados = [ {
+    id: uuidv4(),
+    Nome: 'amaciante'
+},{
+    id: uuidv4(),
+    Nome: 'shampoo'
+},{
+    id: uuidv4(),
+    Nome: 'carvao'
+},{
+    id: uuidv4(),
+    Nome: 'vassoura'
+}]
+
 app.get('/', (req, res) => {
-  res.send('Obtendo Lista de produto')
+  res.json(dados)
 })
 app.get('/:id', (req, res) => {
-  res.send(`listando produto ${req.params.id}`);
+    let p = dados.filter( x=> x.id == req.params.id)
+
+  res.send(p);
 })
 app.post('/', function (req, res) {
-    res.send('Inserindo um produto');
+    let p = {
+        id: uuidv4(),
+        Nome: req.body
+    }; 
+    dados.push(p); 
+    res.send(`Produto inserido!`);
   });
 
 app.put('/:id', function (req, res) {
-res.send(`Atualizando produto ${req.params.id}`);
+    let p = req.body;
+    
+    dados = dados.splice(dados.findIndex(x => x.id == req.params.id));
+
+    dados.push(p);
+
+    res.send(dados);
 });
 
-app.delete('/:', function (req, res) {
-    res.send(`Deletando produto ${req.params.id}`);
+app.delete('/:id', function (req, res) {
+
+    dados = dados.filter(x => x.id != req.params.id)
+
+    res.send(dados).status(200);
 });
   
 app.listen(port, () => {
