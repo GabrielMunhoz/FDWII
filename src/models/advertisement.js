2c5fc1c313d6073ddde9a654c14f4c15df5d0d03
gabrielmunhoz <gabriell_munhoz@icloud.com>
2022-10-13 21:49:36 -0300

    Revalidado retornos e adicionado validacoes no post advertisement

--- src/models/advertisement.js
+++ src/models/advertisement.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const AdvertisementSchema = new mongoose.Schema(
   {
-    gameCategory: [{ type: mongoose.Schema.Types.ObjectId, ref: "GameCategory" }],
+    gameCategory: { type: mongoose.Schema.Types.ObjectId, ref: "GameCategory" },
     groupCategory: String,
     isActive: Boolean,
-    playerHost: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }],
+    playerHost: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
     guests: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }],
   },
   { versionKey: false },
